d553fe4263ef59d9ad861d17e9e4b3da82059596
Dioni00 <dsanbla@gmail.com>
2021-02-25 17:10:41 +0100

    Stage 5. Get information from FloatRates to get the current change for the coin

--- CC_floatrates.py
+++ CC_floatrates.py
@@ -1,13 +1,13 @@
-import requests
-import json
+from requests import get
+from json import loads
 
 url = "http://www.floatrates.com/daily/" + input().lower() + ".json"
 
-# Get the content of de JSON from website
-r = requests.get(url).content
+# Get the JSON content from website
+r = get(url).content
 
 # Load the json STRING (loads) to create the dictionary
-dict_coins = json.loads(r)
+dict_coins = loads(r)
 
 print(dict_coins["usd"])
 print(dict_coins["eur"])
